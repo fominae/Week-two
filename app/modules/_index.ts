f154fecb2680ebb60966497e7e730f8bc403de08
fominae <fomina.elena04@mail.ru>
2025-01-24 01:29:30 +0700

    feature (objective): adding objective router

--- app/modules/_index.ts
+++ app/modules/_index.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import { userRouter } from "./user/router.user";
+import { objectiveRouter } from "./objective/router.objective";
 
 interface IProvider {
     instance: (app: FastifyInstance) => Promise<void>;
     prefix: string;
 }
 
-export const HttpProvider: IProvider[] = [{ instance: userRouter, prefix: "user" }];
+export const HttpProvider: IProvider[] = [{ instance: userRouter, prefix: "user" },{ instance: objectiveRouter, prefix: "to-do" }];
+

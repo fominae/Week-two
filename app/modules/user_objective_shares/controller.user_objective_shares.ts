9f6f92a00b440f1eb27229b7e78200470a7961cb
fominae <fomina.elena04@mail.ru>
2025-01-31 01:03:10 +0700

    feature (user objective share): add nodemailer for sending email to user

--- app/modules/user_objective_shares/controller.user_objective_shares.ts
+++ app/modules/user_objective_shares/controller.user_objective_shares.ts
@@ -1,19 +1,38 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { sqlCon } from "../../common/config/kysely-config";
+import { sendEmail } from "../../common/config/nodemailer";
 import { HttpStatusCode } from "../../common/enum/http-status-code";
 import { uuidSchema } from "../objective/schemas/uuid.schema";
+import * as userRepository from "../user/repository.user";
 import * as shareRepository from "./repository.user_objective_shares";
 import { ShareObjectiveInput } from "./schemas/user_objective_shares.schema";
 
 export async function shareObjective(req: FastifyRequest<{ Body: ShareObjectiveInput; Params: { id: string } }>, rep: FastifyReply) {
     const { id: objectiveid } = req.params;
     const { userid } = req.body;
+    const user = await userRepository.getById(sqlCon, userid);
+    if (!user) {
+        return rep.code(HttpStatusCode.NOT_FOUND).send({ message: "Пользователь не найден" });
+    }
+    if (req.user.id === userid) {
+        return rep.code(HttpStatusCode.BAD_REQUEST).send({ message: "Нельзя предоставить доступ самому себе" });
+    }
+    if (user?.email) {
+        await sendEmail(user.email, "Доступ к задаче предоставлен", `Вы получили доступ к задаче: ${objectiveid}.`);
+    }
     await shareRepository.shareObjective(sqlCon, { userid, objectiveid });
     return rep.code(HttpStatusCode.OK).send({ message: "Доступ выдан" });
 }
-
-export async function revokeObjectiveAccess(req: FastifyRequest<{ Params: uuidSchema }>, rep: FastifyReply) {
+export async function revokeObjectiveAccess(req: FastifyRequest<{ Body: ShareObjectiveInput; Params: uuidSchema }>, rep: FastifyReply) {
     const { id } = req.params;
+    const { userid } = req.body;
+    const user = await userRepository.getById(sqlCon, userid);
+    if (!user) {
+        return rep.code(HttpStatusCode.NOT_FOUND).send({ message: "Пользователь не найден" });
+    }
+    if (req.user.id === userid) {
+        return rep.code(HttpStatusCode.BAD_REQUEST).send({ message: "Нельзя удалить доступ у самого себя" });
+    }
     await shareRepository.revokeAccess(sqlCon, id);
     return rep.code(HttpStatusCode.OK).send({ message: "Доступ удалён" });
 }
